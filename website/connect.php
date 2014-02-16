7e12f0254ed26c5bca735bbae9df6446773e814f
asolanas <agusti.solanas@gmail.com>
2014-02-16 06:06:18 +0100

    Update connect.php

--- website/connect.php
+++ website/connect.php
@@ -1,5 +1,5 @@
 <?php
 	mysql_set_charset('utf8');
-	$conectar = mysql_connect('localhost', 'mcarer', 'mcarer808080') or die('No se puede conectar al servidor.' );
+	$conectar = mysql_connect('localhost', 'mcarer', '******') or die('No se puede conectar al servidor.' );
 	mysql_select_db('mcarer_db', $conectar) or die('No se puede conectar con la base de datos.');
 ?>
